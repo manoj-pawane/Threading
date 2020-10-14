a5282dc738e509e29c5b25dfb4b0136be07d68e3
Manoj <manoj.pawane@bridgelabz.com>
2020-10-14 09:37:16 +0530

    [Manoj] Add . Threading code added

---
+++ ServerClass/CreateThread.cs
@@ -0,0 +1,33 @@
+﻿namespace ServerClass
+{
+    using System;
+    using System.Threading;
+
+    public class CreateThread
+    {
+        public void InstanceMethod()
+        {
+            Console.WriteLine(
+                "Program.InstanceMethod is running on another thread.");
+
+            // Pause for a moment to provide a delay to make
+            // threads more apparent.
+            Thread.Sleep(3000);
+            Console.WriteLine(
+                "The instance method called by the worker thread has ended.");
+        }
+
+        public static void StaticMethod()
+        {
+            Console.WriteLine(
+                "Program.StaticMethod is running on another thread.");
+
+            // Pause for a moment to provide a delay to make
+            // threads more apparent.
+            Thread.Sleep(5000);
+            Console.WriteLine(
+                "The static method called by the worker thread has ended.");
+        }
+    }
+}
+    
---
+++ ServerClass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateThread createThread = new CreateThread();
+
+            // Create the thread object, passing in the
+            // serverObject.InstanceMethod method using a
+            // ThreadStart delegate.
+            Thread InstanceCaller = new Thread(
+                new ThreadStart(createThread.InstanceMethod));
+
+            // Start the thread.
+            InstanceCaller.Start();
+
+            Console.WriteLine("The Main() thread calls this after "
+                + "starting the new InstanceCaller thread.");
+
+            // Create the thread object, passing in the
+            // serverObject.StaticMethod method using a
+            // ThreadStart delegate.
+            Thread StaticCaller = new Thread(
+                new ThreadStart(CreateThread.StaticMethod));
+
+            // Start the thread.
+            StaticCaller.Start();
+
+            Console.WriteLine("The Main() thread calls this after "
+                + "starting the new StaticCaller thread.");
+        }
+
+    }
+}
